5839f2b85f91bcc683b1cef135c703601d26fe0b
Jaap Langemeijer <jaaplangemeijer@gmail.com>
2022-12-16 10:20:36 +0100

    fix tests with new Error

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 import pytest
 
+from eo_bathymetry_functions.exceptions import ArgumentError
 from eo_bathymetry_functions.utils import get_rolling_window_dates
 
 def test_get_rolling_window_dates_2periods():
@@ -8,9 +9,9 @@ def test_get_rolling_window_dates_2periods():
         ("2018-01-01", "2020-01-01"), ("2018-04-01", "2020-04-01")]
 
 def test_get_rolling_window_dates_periods_too_close():
-    with pytest.raises(RuntimeError):
+    with pytest.raises(ArgumentError):
         get_rolling_window_dates("2015-01-01", "2016-01-01")
 
 def test_get_rolling_window_dates_start_stop_reversed():
-    with pytest.raises(RuntimeError):
+    with pytest.raises(ArgumentError):
         dates = get_rolling_window_dates("2020-04-01", "2018-01-01")
